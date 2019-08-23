aea944fb254efdd5b0cf49ab5e34bbaabfd08cc0
quietQuiet <j.lietunovas@gmail.com>
2019-08-23 18:12:50 +0200

    add some pages to nav bar (visually)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -40,10 +40,10 @@ const Navbar = class extends React.Component {
         aria-label="main-navigation"
       >
         <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
+          <div className="navbar-brand ">
+            {/* <Link to="/about" className="right" title="Logo">
+              WAREHOUSE
+            </Link> */}
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
@@ -60,32 +60,29 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/">
+                All
               </Link>
               <Link className="navbar-item" to="/events">
                 Events
               </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
+              <Link className="navbar-item" to="/podcast">
+                Podcast
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+              <Link className="navbar-item" to="/shop">
+                Shop
               </Link>
+
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
+                <Link to="/about" className="right" title="Logo">
+                  WAREHOUSE
+                </Link>
               </a>
             </div>
           </div>
