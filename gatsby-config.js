d042239ee97abf4df6f890a737249fa6302c864f
quietQuiet <j.lietunovas@gmail.com>
2019-08-23 17:22:57 +0200

    change  the landing page to contain only events

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Warehouse',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is the Warehouse website built for E by Zuzana and Jurgis',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
