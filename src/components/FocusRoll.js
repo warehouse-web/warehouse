cc7eb39b558140928cd814c788be7321db5fb8ae
Jurgis <jurgis.lietunovas@random.studio>
2019-10-28 22:40:06 +0100

    MVP pdf

---
+++ src/components/FocusRoll.js
@@ -0,0 +1,162 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import { graphql, StaticQuery } from 'gatsby'
+import './main.css'
+import Img from 'gatsby-image'
+import DivOverlay from '../templates/DivOverlay'
+import Content, { HTMLContent, useMedia } from '../components/utils'
+import { Page, Image,  Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#E4E4E4'
+    },
+    image: { flexGrow: 8, backgroundColor: 'grey', padding: 10 },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1
+    }
+  });
+
+  const MyDocument = ({activeFocus}) => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+        {activeFocus.frontmatter.intro.blurbs.map(el => {return(
+            <>
+        <Text>{el.text}</Text>
+        <Image src ={el.image.childImageSharp.fluid.src} style={styles.image}/>
+
+        </>
+        )})}
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+
+const FocusRoll = ({data}) => {
+  const [activeFocus, setActiveFocus] = useState({})
+  const [showFocusDetail,setShowFocusDetail] = useState(false)
+
+  const openFocus = (focus) => {
+    focus &&
+    window.history.pushState(
+      {page: 1},
+      focus.frontmatter.title,
+      `?focus=${focus.frontmatter.title}`
+    );
+    if (focus !== !!activeFocus) {
+      setActiveFocus(focus)
+      setShowFocusDetail(true);
+      }
+    }
+
+    const PostContent = HTMLContent || Content
+    const match = useMedia("(max-width: 900px) ");
+    const { edges: posts } = data.allMarkdownRemark
+
+    return (
+    <>
+    <DivOverlay />
+    <div className="wrapper">
+      <div className="article-list">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <div key={post.id}>
+              <article
+                onClick={() => openFocus(post)}
+                className={`blog-list-item post`}
+              >
+                <h2 className='post-type'>Focus</h2>
+                <header>
+                  <p className="post-meta">
+                      {post.frontmatter.title}
+                      {post.frontmatter.location}
+                  </p>
+                </header>
+
+              </article>
+            </div>
+          ))}
+          { !posts &&
+            <h1>No Focus To Show ... Yet</h1>
+          }
+    </div>
+    {showFocusDetail && (
+        <div className={`article-detail ${match ? `mobile` : ``}`}>
+            <div className='close' onClick={() => setShowFocusDetail(false)}>
+              <span></span>
+              <span></span>
+            </div>
+          <p className="article-ID">{activeFocus.frontmatter.warehouseID}</p>
+          <h2 className="article-detail-title">{activeFocus.frontmatter.title}</h2>
+            <PDFViewer>
+            <MyDocument activeFocus={activeFocus} />
+            </PDFViewer>
+          {<PostContent className = 'content' content={activeFocus.html} />}
+
+          {activeFocus.excerpt}
+        </div>
+      )}
+    </div>
+    </>
+  )
+}
+
+
+FocusRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+    query FocusRollQuerry {
+        allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "focus-page"}}}) {
+          edges {
+            node {
+              id
+              html
+              fields {
+                slug
+              }
+              frontmatter {
+                warehouseID
+                title
+                intro {
+                  blurbs {
+                    image {
+                      childImageSharp {
+                        fluid {
+                          src
+                        }
+                      }
+                      relativePath
+                      absolutePath
+                      id
+                    }
+                    text
+                  }
+                }
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                podcastURL
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <FocusRoll data={data} count={count} />}
+  />
+)
+
---
+++ src/templates/focus-page.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/utils'
+import '../components/main.css'
+
+export const FocusPageTemplate = ({
+  content,
+  contentComponent,
+  location,
+  date,
+  // tags,
+  title,
+  warehouseID,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <div className="event-detail">
+      {helmet || ''}
+            <p style={{
+              fontSize: '.7rem',
+              fontFamily: 'Arial, Helvetica, sans-serif',
+              textAlign: 'center'
+              }}
+            >
+              {warehouseID}
+            </p>
+            <h2 style={{
+              textAlign:'center',
+              fontSize: '2em',
+              margin: '0 2.5rem 1rem',
+              lineHeight: '1.1',
+            }}>
+              {title}
+            </h2>
+            <PostContent
+             style = {{
+               justifyContent: 'left',
+               textAlign:'left',
+               margin: '3.8em',
+               marginTop: '1.5rem'
+             }} className='content' content={content} />
+    </div>
+  )
+}
+
+FocusPageTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const FocusPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <FocusPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        location = {post.frontmatter.location}
+        date = {post.frontmatter.date}
+        warehouseID = {post.frontmatter.warehouseID}
+        // tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  )
+}
+
+FocusPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default FocusPage
+
+export const pageQuery = graphql`
+  query FocusPageByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        warehouseID
+        title
+        location
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
