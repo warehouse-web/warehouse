fcbc98f730d69d3eaaaf3c42cd63c3019fdddabb
Jurgis <jurgis.lietunovas@random.studio>
2020-04-05 14:35:27 +0200

    changes to the build process

--- src/cms/preview-templates/EventPostPreview.js
+++ src/cms/preview-templates/EventPostPreview.js
@@ -2,8 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import EventDetail from "../../components/EventDetail";
 
-const EventPostPreview = ({ entry }) => {
-	return <EventDetail {...entry.toJSON().data} />;
+const EventPostPreview = ({ entry, getAsset }) => {
+	console.log("getAsset:", getAsset());
+
+	return (
+		<EventDetail
+			image={getAsset(entry.getIn(["data", "image"]))}
+			{...entry.toJSON().data}
+		/>
+	);
 };
 
 EventPostPreview.propTypes = {
