105e5263c88e0ff6352efd45b08953675f8a9c38
nejurgis <jurgis.lietunovas@random.studio>
2020-04-18 11:21:57 +0200

    deploy lambda from egghead tut

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = {
+	developMiddleware: app => {
+		app.use(
+			"/.netlify/functions/",
+			createProxyMiddleware({
+				target: "http://localhost:9000",
+				pathRewrite: {
+					"/.netlify/functions/": ""
+				}
+			})
+		);
+	},
 	siteMetadata: {
 		title: "Warehouse",
 		description:
@@ -84,18 +95,7 @@ module.exports = {
 		},
 		"gatsby-remark-embed-soundcloud",
 		"gatsby-plugin-netlify" // make sure to keep it last in the array
-	],
+	]
 	// for avoiding CORS while developing Netlify Functions locally
 	// read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-	developMiddleware: app => {
-		app.use(
-			"/.netlify/functions/",
-			createProxyMiddleware({
-				target: "http://localhost:9000",
-				pathRewrite: {
-					"/.netlify/functions/": ""
-				}
-			})
-		);
-	}
 };
