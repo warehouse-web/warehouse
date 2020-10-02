3d147c92d39dd17c41589c8e9747ac317da87921
Eurico Sá Fernandes <mail@euricosafernandes.com>
2020-10-02 19:45:47 +0200

    remove price if not assigned

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -31,7 +31,11 @@ const Detail = ({
 			<h2 className="article-detail-title">
 				{active ? active.frontmatter.title : title ? title : ""}
 			</h2>
-			<div className="btn">Add to cart</div>
+			{price && (
+				<div className="article-detail-price">
+					<div className="btn">Add to cart</div>
+				</div>
+			)}
 			<section className="content">
 				{((active && active.frontmatter.content) || []).map((el, i) => {
 					if (el.type === "images") {
