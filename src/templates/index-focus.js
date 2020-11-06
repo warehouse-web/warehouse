fd712c0a3f356afc4f3bacdba9e5fc08febd6316
Eurico Sá Fernandes <mail@euricosafernandes.com>
2020-11-06 16:14:41 +0100

    fix PDF

--- src/templates/index-focus.js
+++ src/templates/index-focus.js
@@ -53,7 +53,7 @@ export default () => (
 									caption
 									body
 								}
-								PDF
+								PDF { ... }
 							}
 						}
 					}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -77,7 +77,7 @@ export default () => (
 									caption
 									body
 								}
-								PDF
+								PDF { ... }
 							}
 						}
 					}
