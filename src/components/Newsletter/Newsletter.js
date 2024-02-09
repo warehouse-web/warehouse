ea0a9daa963ddf5756f1c86daa0a4854cf950a59
Eurico Sá Fernandes <1779775+eeeurico@users.noreply.github.com>
2024-02-09 12:52:56 +0100

    newsletter fix

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,29 +1,12 @@
-import Mailchimp from 'react-mailchimp-form'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 const Newsletter = () => {
+	const url =
+		'//thisiswarehouse.us5.list-manage.com/subscribe/post?u=399b6f2fb11e21f440e25e7f2&amp;id=3684c37c49'
 	return (
 		<div className='Newsletter'>
 			<p>Newsletter</p>
-
-			<Mailchimp
-				action='https://thisiswarehouse.us5.list-manage.com/subscribe/post?u=399b6f2fb11e21f440e25e7f2&amp;id=3684c37c49'
-				fields={[
-					{
-						name: 'EMAIL',
-						placeholder: 'Email',
-						type: 'email',
-						required: true,
-					},
-				]}
-				messages={{
-					sending: 'Sending...',
-					success: 'Thank you for subscribing!',
-					error: 'An unexpected internal error has occurred.',
-					empty: 'You must write an e-mail.',
-					duplicate: 'Too many subscribe attempts for this email address',
-					button: 'Subscribe',
-				}}
-			/>
+			<MailchimpSubscribe url={url} />
 		</div>
 	)
 }
